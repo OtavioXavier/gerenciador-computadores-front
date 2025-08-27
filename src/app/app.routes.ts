d2a437ccc30ad46494ff956aa19136081db9f456
OtavioXavier <otavioxavier.santos@ucsal.edu.br>
2025-08-27 00:35:08 -0300

    :sparkles: feat: inital page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Cadastro } from './pages/cadastro/cadastro';
+import { Detalhes } from './detalhes/detalhes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard', component: Dashboard},
+  {path: 'cadastro', component: Cadastro},
+  {path: 'detalhes/:id', component: Detalhes},
+];
